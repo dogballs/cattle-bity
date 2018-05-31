441ba0956120e8f850045e514b9bc02971b94f77
Michael Radionov <micrddrgn@gmail.com>
2018-05-31 21:07:16 +0300

    [Fire] #12 Shell explosion animation

---
+++ src/ShellExplosion.js
@@ -0,0 +1,50 @@
+import Actor from './Actor.js';
+import Animation from './Animation.js';
+import Sprite from './Sprite.js';
+import TextureLoader from './TextureLoader.js';
+
+class ShellExplosion extends Actor {
+  constructor() {
+    super(55, 55);
+
+    this.texture = new TextureLoader().load('images/sprite.png');
+
+    this.animation = new Animation([
+      new Sprite(this.texture, { x: 259, y: 130, w: 11, h: 11 }),
+      new Sprite(this.texture, { x: 273, y: 129, w: 15, h: 15 }),
+      new Sprite(this.texture, { x: 288, y: 128, w: 16, h: 16 }),
+    ], { delay: 100, loop: false });
+
+    // Each sprite fragment has different size. Try to match it with
+    // canvas size for different animation frames.
+    this.dimensions = [
+      { width: 55, height: 55 },
+      { width: 75, height: 75 },
+      { width: 80, height: 80 },
+    ];
+  }
+
+  update() {
+    this.animation.animate();
+  }
+
+  isComplete() {
+    return this.animation.isComplete();
+  }
+
+  render() {
+    const sprite = this.animation.getCurrentFrame();
+    const frameIndex = this.animation.frameIndex;
+
+    const { width, height } = this.dimensions[frameIndex];
+
+    return {
+      width: width,
+      height: height,
+      position: this.position,
+      sprite,
+    };
+  }
+}
+
+export default ShellExplosion;
