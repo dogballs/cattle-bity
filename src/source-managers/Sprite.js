8d4f600ee06706818ae989eca08c6893232aeb84
Michael Radionov <micrddrgn@gmail.com>
2018-06-01 22:11:50 +0300

    [Render] Remove unnecessary Rect class

--- src/source-managers/Sprite.js
+++ src/source-managers/Sprite.js
@@ -1,8 +1,7 @@
-import Rect from '../canvas/Rect.js';
 import Texture from './Texture.js';
 
 class Sprite {
-  constructor(texture = new Texture(), bounds = new Rect()) {
+  constructor(texture = new Texture(), bounds = { x: 0, y: 0, w: 0, h: 0 }) {
     this.texture = texture;
     this.bounds = bounds;
   }
