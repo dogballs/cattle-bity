5b76451e77c7fee16677038e75a1994e848d0a61
Michael Radionov <micrddrgn@gmail.com>
2018-06-02 15:22:37 +0300

    feat(spawn): Create animation
    Closes #19

--- src/core/Animation.js
+++ src/core/Animation.js
@@ -1,57 +1,86 @@
 class Animation {
   constructor(frames = [], { delay = 0, loop = true } = {}) {
     this.frames = frames;
-    this.delay = delay;
-    this.loop = loop;
-
-    this.lastAnimatedAt = performance.now();
     this.frameIndex = 0;
-  }
 
-  animate() {
-    const now = performance.now();
+    this.delay = delay;
+    this.lastAnimatedAt = performance.now();
 
-    // Debounces animation to create a delay between frames if #animate is called
-    // too fast.
-    // TODO: rethink if it belongs here.
-
-    // Only check delay if it is set
-    if (this.delay > 0) {
-      // If enough time has not passed yet from the last animation - just wait
-      if (this.lastAnimatedAt + this.delay > now) {
-        return;
-      }
-    }
-
-    this.frameIndex += 1;
-    if (this.frameIndex > this.frames.length - 1) {
-      this.frameIndex = 0;
-    }
-
-    this.lastAnimatedAt = now;
+    this.loop = loop;
+    this.loopIndex = 1;
   }
 
   getCurrentFrame() {
     return this.frames[this.frameIndex];
   }
 
+  animate() {
+    // If looping is disabled and last frame animation is complete - nothing else
+    // to animate
+    if (this.loop === false
+      && this.isLastFrame()
+      && this.isCurrentFrameAnimated()
+    ) {
+      return;
+    }
+
+    // If loop is limited to number and we are on the last loop and last frame
+    // animation is complete - nothing else to animate
+    if (typeof this.loop === 'number'
+      && this.loopIndex === this.loop
+      && this.isLastFrame()
+      && this.isCurrentFrameAnimated()
+    ) {
+      return;
+    }
+
+    // Debounces animation to create a delay between frames.
+    // If enough time has not passed yet from the last animation - do nothing.
+    if (!this.isCurrentFrameAnimated()) {
+      return;
+    }
+
+    // Go to the next frame
+
+    this.frameIndex += 1;
+    if (this.frameIndex > this.frames.length - 1) {
+      this.frameIndex = 0;
+      // When frame is reset to the first one, consider one animation loop complete
+      this.loopIndex += 1;
+    }
+
+    this.lastAnimatedAt = performance.now();
+  }
+
+  isCurrentFrameAnimated() {
+    const now = performance.now();
+    const isAnimated = this.lastAnimatedAt + this.delay < now;
+    return isAnimated;
+  }
+
+  isLastFrame() {
+    return this.frameIndex === this.frames.length - 1;
+  }
+
   isComplete() {
     // Looped animation can not end
-    if (this.loop) {
+    if (this.loop === true) {
       return false;
     }
 
-    const isLastFrame = this.frameIndex === this.frames.length - 1;
-    if (!isLastFrame) {
+    // If loop is limited by number of times and it hasn't looped all times yet
+    // animation is not complete.
+    if (typeof this.loop === 'number' && this.loopIndex !== this.loop) {
+      return false;
+    }
+
+    if (!this.isLastFrame()) {
       return false;
     }
 
     // Make sure last frame is displayed on the screen for required amount
     // of time. Only after that set animation as complete.
-    const now = performance.now();
-    const isLastFrameComplete = this.lastAnimatedAt + this.delay < now;
-
-    const isComplete = isLastFrameComplete;
+    const isComplete = this.isCurrentFrameAnimated();
 
     return isComplete;
   }
