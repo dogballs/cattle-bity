f13e4e6957905ddeb9d2c9dc9083b437f3477437
Michael Radionov <micrddrgn@gmail.com>
2020-05-13 15:07:45 +0300

    fix(config): Revert level start delay. Up friendly fire stun duration.

--- src/config.ts
+++ src/config.ts
@@ -69,13 +69,13 @@ export const SHIELD_POWERUP_DURATION = 10;
 export const BASE_DEFENCE_POWERUP_DURATION = 17;
 export const FREEZE_POWERUP_DURATION = 10;
 
-export const FRIENDLY_FIRE_STUN_DURATION = 2;
+export const FRIENDLY_FIRE_STUN_DURATION = 5;
 export const ICE_SLIDE_DURATION = 0.5;
 
 export const POINTS_POWERUP_DURATION = 0.8;
 export const POINTS_ENEMY_TANK_DURATION = 0.16;
 
-export const LEVEL_START_DELAY = 5;
+export const LEVEL_START_DELAY = 2;
 
 export const PLAYER_INITIAL_LIVES = 3;
 export const PLAYER_EXTRA_LIVE_POINTS = 20000;
